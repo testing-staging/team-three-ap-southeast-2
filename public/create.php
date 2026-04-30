0b0588621eafb3bf85303d0196a29aa83ed10d6b
Webslice Developers <developers@webslice.com>
2026-04-30 03:40:18 +0000

    Webslice Automated Test: Deploy Test

--- public/create.php
+++ public/create.php
@@ -1,9 +1,1 @@
-<?php
-if ($_GET['file'] == "") {
-    echo "Success";
-} else {
-    file_put_contents($_GET['file'], "<?php\n\necho \"Success\";\n\n?>");
-    echo "Success";
-}
-?>
-
+<?= "3f1816dcea0fa270360c57938bb30833"; ?>
